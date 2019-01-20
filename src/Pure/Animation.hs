8042feba9e447786df27cf3eeb4efe5a8f32f185
grumply <sean@grump.ly>
2019-01-20 07:48:20 -0600

    Fix worker death bug.

--- src/Pure/Animation.hs
+++ src/Pure/Animation.hs
@@ -1,40 +1,46 @@
-{-# LANGUAGE CPP #-}
-module Pure.Animation (addAnimation,addAnimations,addAnimationsReverse) where
+{-# LANGUAGE CPP, ScopedTypeVariables #-}
+{-# OPTIONS_GHC -fno-full-laziness -fno-state-hack #-}
+module Animation (addAnimation,addAnimations,addAnimationsReverse) where
 
+import Control.Exception (SomeException,catch)
 import Control.Concurrent (MVar,newEmptyMVar,forkIO,takeMVar,putMVar,tryPutMVar)
 import Control.Monad (void,forever)
+import Data.Function (fix)
 import Data.IORef (IORef,newIORef,atomicModifyIORef')
 import System.IO.Unsafe (unsafePerformIO)
 
+import Pure.Data.Txt (Txt,toTxt)
+
 -- from ghcjs-base
 #ifdef __GHCJS__
-import GHCJS.Foreign.Callback (syncCallback1,OnBlocked(..),releaseCallback)
+import GHCJS.Foreign.Callback (Callback,syncCallback1,OnBlocked(..),releaseCallback)
+import GHCJS.Concurrent
 #endif
 
 -- from pure-core (local)
-import Pure.Data.Lifted (requestAnimationFrame)
+import Pure.Data.Lifted (JSV)
 
 -- TODO: Is there a way to do this without the MVar
 --       and without calling requestAnimationFrame
 --       continuously?
-
-{-# NOINLINE addAnimation #-}
+{-# INLINE addAnimation #-}
 addAnimation :: IO () -> IO Bool
 addAnimation a = animator `seq` do
     atomicModifyIORef' animationQueue $ \as -> (a:as,())
     tryPutMVar animationsAwaiting ()
 
-{-# NOINLINE addAnimations #-}
+{-# INLINE addAnimations #-}
 addAnimations :: [IO ()] -> IO Bool
 addAnimations new = animator `seq` do
     atomicModifyIORef' animationQueue $ \as -> (reverse new ++ as,())
     tryPutMVar animationsAwaiting ()
 
-{-# NOINLINE addAnimationsReverse #-}
+{-# INLINE addAnimationsReverse #-}
 addAnimationsReverse :: [IO ()] -> IO Bool
 addAnimationsReverse new = animator `seq` do
     atomicModifyIORef' animationQueue $ \as -> (new ++ as,())
-    tryPutMVar animationsAwaiting ()
+    b <- tryPutMVar animationsAwaiting ()
+    return b
 
 {-# NOINLINE animationsAwaiting #-}
 animationsAwaiting :: MVar ()
@@ -46,32 +52,34 @@ animationQueue = unsafePerformIO (newIORef [])
 
 {-# NOINLINE animator #-}
 animator :: ()
-animator = unsafePerformIO $ void $ forkIO await
+animator = unsafePerformIO $ void $ forkIO (fix (work >>))
   where
-    await :: IO ()
-    await = forever $ do
-        takeMVar animationsAwaiting
-        as <- atomicModifyIORef' animationQueue $ \as -> ([],as)
-        animate as
-
-{-# INLINE (<<) #-}
-(<<) :: IO b -> IO a -> IO b
-(<<) = flip (>>)
+    work = (fix (await >>)) `catch` \(_ :: SomeException) -> return ()
+      where
+        await :: IO ()
+        await = do
+          takeMVar animationsAwaiting
+          as <- atomicModifyIORef' animationQueue $ \as -> ([],as)
+          animate (reverse as)
 
 animate :: [IO ()] -> IO ()
 animate as = do
 #ifdef __GHCJS__
     barrier <- newEmptyMVar
-    requestAnimationFrame $ \_ -> do
-      run as
-      putMVar barrier ()
+    let work _ = synchronously $ sequence_ as >>= putMVar barrier
+    callback <- syncCallback1 ContinueAsync work
+    requestAnimation callback
     takeMVar barrier
+    releaseCallback callback
 #else
-    run as
+    sequence_ as
 #endif
-  where
-    {-# INLINE run #-}
-    run = foldr (<<) (return ())
 
+#ifdef __GHCJS__
+foreign import javascript unsafe
+  "$r = window.requestAnimationFrame($1)" request_animation_frame_js :: Callback (JSV -> IO ()) -> IO JSV
 
-
+{-# INLINE requestAnimation #-}
+requestAnimation :: Callback (JSV -> IO ()) -> IO ()
+requestAnimation cb = void $ request_animation_frame_js cb
+#endif
--- src/Pure/IdleWork.hs
+++ src/Pure/IdleWork.hs
@@ -1,8 +1,10 @@
-{-# LANGUAGE CPP, BangPatterns #-}
-module Pure.IdleWork (addIdleWork,addIdleWorks,addIdleWorksReverse) where
+{-# LANGUAGE CPP, BangPatterns, ScopedTypeVariables #-}
+{-# OPTIONS_GHC -fno-full-laziness -fno-state-hack #-}
+module IdleWork (addIdleWork, addIdleWorks, addIdleWorksReverse) where
 
 import Control.Concurrent (MVar,newEmptyMVar,forkIO,takeMVar,putMVar,tryPutMVar,yield,threadDelay)
 import Control.Monad (forever,join,void)
+import Data.Foldable
 import Data.Function (fix)
 import Data.IORef (IORef,newIORef,readIORef,writeIORef,atomicModifyIORef')
 import Data.List (intersperse)
@@ -13,12 +15,14 @@ import Pure.Data.Lifted (JSV)
 -- from ghcjs-base
 #ifdef __GHCJS__
 import GHCJS.Foreign.Callback (syncCallback1,OnBlocked(..),releaseCallback)
-#else
-import System.CPUTime
+import GHCJS.Concurrent (synchronously)
 #endif
 
 -- from pure-core (local)
 import Pure.Data.Lifted (requestIdleCallback)
+
+import Control.Exception (SomeException,catch)
+import Pure.Data.Txt (Txt,toTxt)
 
 -- |
 -- GHCJS: We rely on a poly-filled version of requestIdleCallback
@@ -27,50 +31,25 @@ import Pure.Data.Lifted (requestIdleCallback)
 -- but we check if a reschedule is necessary between each action and act
 -- accordingly, cleaning up callbacks as we go.
 --
--- 
--- WARNING: Don't rely on idleWorker with GHC. Until (if) prioritized 
---          threads land in GHC, idleWorker will not work correctly. 
+-- GHC: All actions are run in a forked thread that yields often.
 --
--- GHC: We try to exploit the round-robin nature of thread scheduling
--- to detect and exploit idle time. On entry we take the current time
--- and immediately yield. On re-entry, we take the time and compare it
--- with the initial entry time. If the entry and re-entry time differ
--- by less than 2.5 microseconds, we perform one action and then start 
--- over with the next work item in the queue. If it is more than 2.5 
--- microseconds, we start over. If 100 iterations pass without a 
--- detectable idle time, we assume load is high and simply pile on to 
--- try to avoid being the source of a memory leak. In the worst case, 
--- this should be /approximately/ the same as not calling addIdleWork
--- since there is a yield between evaluations of work items from the 
--- queue. In the best case, it will exploit idle time and improve 
--- server response times.
---
--- Best practices:
---   1. Keep in mind that the idle worker can keep gc objects alive longer
---   2. Avoid thread synchronization with the idle worker via MVar barriers except for (4)
---   3. Avoid using idle worker for loads with ACID-like expectations
---   4. When relying on the idle worker, use a synchronizing MVar during shutdown
---      to guarantee that work finished; the idle work queue is FIFO.
---   5. Don't add any shutdown methods as idle work except for (4) when the consumer 
---      is blocking the main thread
---
+{-# INLINE addIdleWork #-}
 addIdleWork :: IO () -> IO Bool
 addIdleWork a = idleWorker `seq` do
     atomicModifyIORef' idleWorkQueue $ \as -> (a:as,())
     tryPutMVar idleWorkAwaiting ()
-{-# NOINLINE addIdleWork #-}
 
+{-# INLINE addIdleWorks #-}
 addIdleWorks :: [IO ()] -> IO Bool
 addIdleWorks new = idleWorker `seq` do
     atomicModifyIORef' idleWorkQueue $ \as -> (reverse new ++ as,())
     tryPutMVar idleWorkAwaiting ()
-{-# NOINLINE addIdleWorks #-}
 
+{-# INLINE addIdleWorksReverse #-}
 addIdleWorksReverse :: [IO ()] -> IO Bool
 addIdleWorksReverse new = idleWorker `seq` do
     atomicModifyIORef' idleWorkQueue $ \as -> (new ++ as,())
     tryPutMVar idleWorkAwaiting ()
-{-# NOINLINE addIdleWorksReverse #-}
 
 {-# NOINLINE idleWorkAwaiting #-}
 idleWorkAwaiting :: MVar ()
@@ -82,65 +61,44 @@ idleWorkQueue = unsafePerformIO (newIORef [])
 
 {-# NOINLINE idleWorker #-}
 idleWorker :: ()
-idleWorker = unsafePerformIO $ void $ forkIO await
+idleWorker = unsafePerformIO $ void $ forkIO (fix (work >>))
   where
-    await :: IO ()
-    await = forever $ do
-        takeMVar idleWorkAwaiting
-        as <- atomicModifyIORef' idleWorkQueue $ \as -> ([],as)
-        workIdly (reverse as)
+    work = (fix (await >>)) `catch` \(_ :: SomeException) -> return ()
+      where
+        await :: IO ()
+        await = do
+          takeMVar idleWorkAwaiting
+          as <- atomicModifyIORef' idleWorkQueue $ \as -> ([],as)
+          workIdly (reverse as)
 
 workIdly :: [IO ()] -> IO ()
-#ifdef __GHCJS__
 workIdly as = do
-    barrier <- newEmptyMVar
-    cb <- newIORef (return ())
-    go cb barrier as
-    join (takeMVar barrier)
-  where
-    go cb barrier = go'
-      where
-        go' as0 = do
-          callback <- syncCallback1 ContinueAsync $ \deadline -> flip fix as0 $ \continue as0 ->
-            case as0 of
-              []  -> readIORef cb >>= putMVar barrier >> writeIORef cb (return ())
-              [a] -> a >> continue []
-              (a0:a1:as) -> do
-                !_  <- a0
-                !tr <- hasTimeRemaining deadline
-                if tr
-                  then continue (a1:as)
-                  else do
-                    rc <- readIORef cb
-                    writeIORef cb (return ())
-                    let a = rc >> a1
-                    go' (a:as)
-          writeIORef cb (releaseCallback callback)
-          void $ requestIdleCallback callback
+#ifndef __GHCJS__
+    yield
+    sequence_ (intersperse yield as)
 #else
-workIdly = go 0
+    wrapper >>= traverse_ workIdly
   where
-    go !_ [] = return ()
-    go n as0@(a:as)
-      -- avoid hogging CPU by yielding as often as possible; at a minimum, this should run every 0.01 seconds
-      | n == 100  = foldr (>>) (return ()) (intersperse yield as0)
-      | otherwise = do
-        !start <- getCPUTime
-        yield
-        !rentry <- getCPUTime
-        let delta = rentry - start
-            cutoff = 2500000 -- 2.5 microsconds
-        if delta < cutoff 
-          then do
-            a
-            go 0 as
-          else do
-            let picoToMicro = flip div 1000000
-            threadDelay (fromIntegral $ picoToMicro delta * 10)
-            go (n + 1) as0
+    wrapper = do
+      barrier <- newEmptyMVar
+      let work deadline = synchronously $ worker deadline as >>= putMVar barrier
+      callback <- syncCallback1 ContinueAsync work
+      requestIdleCallback callback
+      result <- takeMVar barrier
+      releaseCallback callback
+      return result
+      where
+        worker deadline = go
+          where
+
+            go [] = return Nothing
+
+            go (a:as) = do
+              a `catch` \(_ :: SomeException) -> return () -- ignore failed idle workers
+              tr <- hasTimeRemaining deadline
+              let k = if tr then go else return . Just
+              k as
 #endif
-
-
 
 #ifdef __GHCJS__
 foreign import javascript unsafe
