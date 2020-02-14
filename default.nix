53490537b190d608cf954945c7b41fccebee7089
Sean Hickman <sean@grumply.com>
2020-02-13 23:32:45 -0600

    Update for GHC/JS 8.6.5 and 2020

--- default.nix
+++ default.nix
@@ -1,7 +1,7 @@
 { mkDerivation, base, ghcjs-base, pure-default, pure-txt, stdenv }:
 mkDerivation {
   pname = "pure-lifted";
-  version = "0.7.0.0";
+  version = "0.8.0.0";
   src = ./.;
   libraryHaskellDepends = [ base ghcjs-base pure-default pure-txt ];
   homepage = "github.com/grumply/pure-lifted";
